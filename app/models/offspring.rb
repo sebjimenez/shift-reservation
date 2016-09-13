1609b06b5e0142d075093fdc2d3e92c270dcbde6
Adrian Garcia-Vera <adrian.garciavera@es.logicalis.com>
2016-09-13 20:29:57 +0200

    Create offspring model

--- app/models/offspring.rb
+++ app/models/offspring.rb
@@ -1,6 +1,9 @@
 class Offspring < ApplicationRecord
+<<<<<<< abb810f7e75dfcf395609e88570c66193edbf9f0
   belongs_to :user
   validates :first_name, presence: true, length: { within: 2..60 }
   validates :last_name, presence: true, length: { within: 2..60 }
   validates :user, presence: true
+=======
+>>>>>>> Create offspring model
 end
--- spec/factories/offsprings.rb
+++ spec/factories/offsprings.rb
@@ -1,7 +1,13 @@
 FactoryGirl.define do
   factory :offspring do
+<<<<<<< abb810f7e75dfcf395609e88570c66193edbf9f0
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     user
+=======
+    first_name "MyString"
+    last_name "MyString"
+    age 1
+>>>>>>> Create offspring model
   end
 end

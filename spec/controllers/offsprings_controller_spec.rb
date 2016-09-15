8467069395f6c37a99aa5123608f077dcffce17c
Sebastian Jimenez <sebastian.jimenezbenavides@es.logicalis.com>
2016-09-15 17:45:12 +0200

    WIP1 specs

--- spec/controllers/offsprings_controller_spec.rb
+++ spec/controllers/offsprings_controller_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe OffspringsController, type: :controller do
   feature "User access to offsprings", type: :feature do
     FactoryGirl.create(:offspring, first_name: "Sally", last_name: "Jones")
     scenario "visitor views its offsprings" do
-      visit root_path
-      expect(page).to have_content "Sally Jones"
+      visit admin_rooms_path
+      expect(page).to have_content "Add Room"
     end
     scenario "visitor creates a new offspring" do
-      visit add_offspring_path
+      visit offsprings_new_path
       expect(page).to have_content "Add offspring"
     end
     scenario "visitor deletes a offspring" do

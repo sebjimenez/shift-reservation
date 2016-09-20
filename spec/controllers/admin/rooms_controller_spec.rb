b10ad8169e9f10adfcf2c22ec681bb5ffb8a7ff9
Sebastian Jimenez <sebastian.jimenezbenavides@es.logicalis.com>
2016-09-20 21:29:31 +0200

    WIP 3 specs rooms controller

--- spec/controllers/admin/rooms_controller_spec.rb
+++ spec/controllers/admin/rooms_controller_spec.rb
@@ -2,41 +2,43 @@ require 'rails_helper'
 # include Devise::TestHelpers # this should work without this
 
 RSpec.describe Admin::RoomsController, type: :controller do
-
-  feature "Access to view rooms information", type: :feature do
-    context "when logged in as admin" do
-      scenario "when user is not authenticated" do
-      #it has to redirect to a sign_in page
-      end
-      scenario "when user is not authenticated" do
-      #visit admin_rooms_path
-      #expect(page).to have_content "This site is protected. Please log in first."
+  feature "Navigating through rooms,", type: :feature do
+    context "when not logged in as admin" do
+      scenario "trying to access rooms dashboard" do
+        visit admin_rooms_path
+        expect(page).to have_content "You need to sign in or sign up before continuing."
       end
     end
     context "when logged in as admin" do
+      # room = FactoryGirl.create(:room, name: "John", capacity: 12)
+      let(:admin) { FactoryGirl.create(:room, name: "Room ABC", capacity: 12) }
+      let(:admin) { FactoryGirl.create(:admin) }
       before(:each) do
-        admin = FactoryGirl.create(:admin)
-        #login_as(admin , :scope => :admin)
+        sign_in admin
       end
-      scenario "admin searches for a specific user" do
+      scenario "deleting a rooom" do
+        visit admin_rooms_path
+        click_button "Delete room"
+        expect(page).to have_content "Are you sure"
+      end
+      scenario "adding a new rooom" do
+        visit admin_rooms_path
+        # click_button "Add Room"
+        expect(page).to have_content "aaa"
+        # fill_in "Name", :with => "Room Without Name"
+        # fill_in "Capacity", :with => "33"
+      end
+      scenario "updating a rooom" do
+        # go to edit page
+        visit admin_rooms_path
+        # click_button "Room"
+        expect(page).to have_content "Room"
+        # click_button "Edit Room"
+        # visit admin_rooms_path rooms/1/edit
+        # fill_in "Name", :with => "New Name Yeah"
+        # fill_in "Capacity", :with => "34"
+        # click_button "Update Room"
       end
     end
   end
 end
-
-#smith = FactoryGirl.create(:room, name: "John", capacity: 12)
-
-#FactoryGirl.create(:contact, firstname: "Sally", lastname: "Jones")
-
-#visit admin_dashboard_path
-#expect(page).to have_content "Sign In"
-#expect(page).to have_content "Sally Jones"
-
-#let(:admin) { FactoryGirl.build_stubbed(:admin) }
-#post :create, room: {name: "RoomPrueba1", capacity:'12'}
-#expect(response).to render_template(new_admin_session_path)
-
-#expect(current_path).to eq contact_path(smith)
-
-#within "h1" do
-#  expect(page).to have_content "John Smith"

404111ef49b8750af27592a544fedd7997c1bc2b
Sebastian Jimenez <sebastian.jimenezbenavides@es.logicalis.com>
2016-09-23 14:18:14 +0200

    Migrated the new offspring table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920170745) do
+ActiveRecord::Schema.define(version: 20160923121016) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 20160920170745) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.index ["singleton_guard"], name: "index_myconfigs_on_singleton_guard", unique: true
+  end
+
+  create_table "offsprings", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "rooms", force: :cascade do |t|
--- spec/models/offspring_spec.rb
+++ spec/models/offspring_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Offspring, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have attribute type" do
+    expect(subject).to have_attribute :type
+  end
+
+  it "should initialize successfully as an instance of the described class" do
+    expect(subject).to be_a_kind_of described_class
+  end
+
+  it "should identify its parent class" do
+  end
+
+  it "should not exist without parent" do
+    #
+  end
 end

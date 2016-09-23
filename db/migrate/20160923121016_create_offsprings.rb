0dcda9871c56c4a4ab288b9b77cf65dc3f73cae4
Sebastian Jimenez <sebastian.jimenezbenavides@es.logicalis.com>
2016-09-23 14:10:58 +0200

    Generated empty offspring model and spec files

---
+++ db/migrate/20160923121016_create_offsprings.rb
@@ -0,0 +1,10 @@
+class CreateOffsprings < ActiveRecord::Migration[5.0]
+  def change
+    create_table :offsprings do |t|
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps
+    end
+  end
+end

2c3ff65b9d3266d1f5da8e7252ecaa5a4243b76c
Sebastian Jimenez <sebastian.jimenezbenavides@es.logicalis.com>
2016-10-02 13:10:02 +0200

    Fixed rebase conflicts

--- spec/factories/offsprings.rb
+++ spec/factories/offsprings.rb
@@ -1,18 +1,6 @@
 FactoryGirl.define do
   factory :offspring do
-<<<<<<< 8467069395f6c37a99aa5123608f077dcffce17c
-<<<<<<< abb810f7e75dfcf395609e88570c66193edbf9f0
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    user
-=======
-    first_name "MyString"
-    last_name "MyString"
-    age 1
->>>>>>> Create offspring model
-=======
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
->>>>>>> Manually Rebased code with master
   end
 end
